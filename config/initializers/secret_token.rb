38c4ec07b8f5ad332882c09bf7eea9e60d16df47
Elvis Xu <git>
2014-03-08 20:09:32 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrusX::Application.config.secret_token = 'b81a36f32b24189bd18ee8280893c92ebb7159eb22558d0b21b94d98a17c356d3b66cf861df229b00162627d6d833ed1847ab32ba9d8f192cfcd8c6e4d9adbfe'
